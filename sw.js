316db66623d31966578dfd7f99d1f229eacf13ff
themuscleupgym <muscleuphealth@gmail.com>
2025-12-07 20:05:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = 'muscleup-cache-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/user.html',
+  '/trainer.html',
+  '/owner.html',
+  '/manifest.json'
+];
 
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+  );
+});
